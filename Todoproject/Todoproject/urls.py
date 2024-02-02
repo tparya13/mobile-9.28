0a37d70997e9ff1130736cfe58ed734d07943919
arya <aryatheruparambil@gmail.com>
2024-02-02 11:39:05 +0530

    file added

--- Todoproject/Todoproject/urls.py
+++ Todoproject/Todoproject/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('TodoApp.urls')), 
     path('auth/',include('userarauth.urls')), 
+    path('searchapp/',include('searchapp.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
