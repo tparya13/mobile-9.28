28a5f49e29d1c4e00ed046bca50c37b3e9dc2468
arya <aryatheruparambil@gmail.com>
2024-02-08 09:35:50 +0530

    file added

--- Todoproject/cart/urls.py
+++ Todoproject/cart/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 app_name='cart'
 urlpatterns = [
     path('addcart/<int:id>',views.addcart,name='addcart'),
-    path('removecart/',views.removecart,name='removecart'),
-    path('fullremove/',views.fullremove,name='fullremove'),
+    path('removecart/<int:id>',views.removecart,name='removecart'),
+    path('fullremove/<int:id>',views.fullremove,name='fullremove'),
     path('displaycart/',views.displaycart,name='displaycart'),
 ]
